4ff019254db643b5dfda17d14a9ef6619720966a
xodms1701 <kimrrrrt123@naver.com>
2017-04-13 20:24:19 +0900

    #3 Done

--- app/src/main/java/org/techtown/activitytest/SecondActivity.java
+++ app/src/main/java/org/techtown/activitytest/SecondActivity.java
@@ -2,6 +2,8 @@ package org.techtown.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button butBack = (Button)findViewById(R.id.but_back);
+        butBack.setOnClickListener(butBackHandler);
     }
+    View.OnClickListener butBackHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
