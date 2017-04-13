561890f8215dc5d91cd269c7917e83aad9a18ef9
xodms1701 <kimrrrrt123@naver.com>
2017-04-13 20:50:29 +0900

    #5 끝이다 ㅎㅎㅎㅎ

--- app/src/main/java/org/techtown/activitytest/MainActivity.java
+++ app/src/main/java/org/techtown/activitytest/MainActivity.java
@@ -5,20 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editTel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain = (Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMainHandler);
+        editName = (EditText)findViewById(R.id.edit_name);
+        editTel = (EditText)findViewById(R.id.edit_tel);
+
     }
     View.OnClickListener butMainHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            String name = editName.getText().toString();
+            String Tel = editTel.getText().toString();
+            intent.putExtra("name", name);
+            intent.putExtra("tel", Tel);
             startActivity(intent);
         }
     };
--- app/src/main/java/org/techtown/activitytest/SecondActivity.java
+++ app/src/main/java/org/techtown/activitytest/SecondActivity.java
@@ -1,18 +1,25 @@
 package org.techtown.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name, tel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button butBack = (Button)findViewById(R.id.but_back);
         butBack.setOnClickListener(butBackHandler);
+        Intent intent = getIntent();
+        name=intent.getStringExtra("name");
+        tel=intent.getStringExtra("tel");
+        TextView textResult = (TextView)findViewById(R.id.text_result);
+        textResult.setText("*성명 : "+name+"\n *전화번호: "+tel);
     }
     View.OnClickListener butBackHandler = new View.OnClickListener() {
         @Override
